4e19714bb9940dedbd3a9bc69f3fcbdb9f97c7cb
MoatazBellah12 <moatazbellah70@gmail.com>
2021-07-15 17:31:07 +0200

    updates

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,18 @@
-
-$(window).scroll(function () {
-    let scrollValue = $(window).scrollTop();
-    if (scrollValue > 3) {
-        $('nav').css('backgroundColor', '#fff')
-        $('nav ul li a , nav button i').css('color','#000')
-        $('nav .navbar-brand img').attr('src','imgs/logo-red.png')
-    } else {
-        $('nav').css('backgroundColor', 'transparent')
-        $('nav ul li a , nav button i').css('color','#fff')
-        $('nav .navbar-brand img').attr('src','imgs/logo-white.png')
-    }
+$(document).ready(function () {
+    $('#loadingScreen').fadeOut(700, function () {
+        $('body').css('overflow-y', 'auto')
+    })
+    $('p').addClass('lead')
+    $(window).scroll(function () {
+        let scrollValue = $(window).scrollTop();
+        if (scrollValue > 3) {
+            $('nav').css('backgroundColor', '#fff')
+            $('nav ul li a , nav button i').css('color', '#000')
+            $('nav .navbar-brand img').attr('src', 'imgs/logo-red.png')
+        } else {
+            $('nav').css('backgroundColor', 'transparent')
+            $('nav ul li a , nav button i').css('color', '#fff')
+            $('nav .navbar-brand img').attr('src', 'imgs/logo-white.png')
+        }
+    })
 })
